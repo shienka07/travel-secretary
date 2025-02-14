37a677074b356f004a6cc09539d8ba5dc0cef110
YoungBeen <rhdudqls73@gmail.com>
2025-02-14 10:10:02 +0900

    merge

--- mateSearch/js/config.js
+++ mateSearch/js/config.js
@@ -1,5 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const supabase = createClient(
+  "https://frqevnyaghrnmtccnerc.supabase.co",
+  " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZycWV2bnlhZ2hybm10Y2NuZXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyNTk4MDIsImV4cCI6MjA1NDgzNTgwMn0.1ITu6ie5GxJE_mf_a-G5kblLoY_5kYStt6EBxf8drPQ"
+);
 
 export const mateTable = "MATE_POSTING";
 export const tsTable = "TRAVEL_STYLES";
@@ -8,4 +12,3 @@ export const ptsTable = "POSTING_TRAVEL_STYLES";
 export const cmtTable = "POSTING_COMMENTS";
 export const matebucketName = "mate-bucket";
 export const folderName = "mate";
-

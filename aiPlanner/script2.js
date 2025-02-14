1db7094dcddba248087d13144c8e982ac45aafbf
rbxo0128 <rbxo0128@naver.com>
2025-02-14 14:52:34 +0900

    fix aiPlanner nav

---
+++ aiPlanner/script2.js
@@ -0,0 +1,33 @@
+import { checkLogin, logout, getProfile } from "../js/auth";
+
+
+const islogined = await checkLogin();
+  if (!islogined) {
+    window.location.href =
+      "https://aibe-chill-team.github.io/travel-secretary/";
+    alert("로그인이 필요합니다");
+  }
+
+const username = localStorage.getItem("username") || "Guest";
+  document.getElementById("username").textContent = username + " 님";
+
+  if (localStorage.getItem("profile_img")) {
+    const profile_img =
+      "https://frqevnyaghrnmtccnerc.supabase.co/storage/v1/object/public/mate-bucket/" +
+      localStorage.getItem("profile_img");
+    const profile = document.querySelector("#profile");
+    profile.src = profile_img;
+  } else {
+    const data = await getProfile();
+    const profile_img =
+      "https://frqevnyaghrnmtccnerc.supabase.co/storage/v1/object/public/mate-bucket/" +
+      data.image_url;
+    const profile = document.querySelector("#profile");
+    profile.src = profile_img;
+  }
+
+  document.getElementById("logout").addEventListener("click", async (event) => {
+    event.preventDefault();
+    await logout();
+    window.location.href = "../index.html";
+  });

b8284f150925b70d780bb7e5cf9ac9cda8cb8a8f
YoungBeen <rhdudqls73@gmail.com>
2025-02-15 19:33:01 +0900

    fix : 파일경로 수정

---
+++ googleMapApi/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://frqevnyaghrnmtccnerc.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZycWV2bnlhZ2hybm10Y2NuZXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkyNTk4MDIsImV4cCI6MjA1NDgzNTgwMn0.1ITu6ie5GxJE_mf_a-G5kblLoY_5kYStt6EBxf8drPQ";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);

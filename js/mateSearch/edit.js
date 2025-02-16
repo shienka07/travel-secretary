9208fb3f880153ef9ab3fb52cca63befe69fbc57
rbxo0128 <rbxo0128@naver.com>
2025-02-16 21:33:48 +0900

    feat ai 캐릭터, 로그아웃 alert 변경

--- js/mateSearch/edit.js
+++ js/mateSearch/edit.js
@@ -296,7 +296,15 @@ async function initializeEdit(postId) {
       .addEventListener("click", async (event) => {
         event.preventDefault();
         await logout();
-        window.location.href = "../../index.html";
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "로그아웃!\n메인 페이지로 이동합니다.",
+          showConfirmButton: false,
+          timer: 1500
+        }).then(() => {
+          window.location.href = "../../index.html";
+        });
       });
 
     await postingService.checkAuth();

e731af981f685845c89ff9a9bdcd9ded620e7497
rbxo0128 <rbxo0128@naver.com>
2025-02-16 15:35:25 +0900

    fix 파일수정

---
+++ js/aiPlanner/script2.js
@@ -0,0 +1,37 @@
+
+import { checkLogin, logout, getProfile } from "../auth.js";
+
+const islogined = await checkLogin();
+  if (!islogined) {
+    window.location.href =
+      "https://aibe-chill-team.github.io/travel-secretary/";
+    alert("로그인이 필요합니다");
+  }
+
+const username = localStorage.getItem("username") || "Guest";
+  document.getElementById("username").textContent = username + " 님";
+
+  if(localStorage.getItem("profile_img"))
+    {
+        const profile_img = "https://frqevnyaghrnmtccnerc.supabase.co/storage/v1/object/public/mate-bucket/"+ localStorage.getItem("profile_img");
+        const profile = document.querySelector("#profile");
+        profile.src = profile_img;
+    }
+    else{
+        const data = await getProfile();
+        
+        if(!data.image_url == ""){
+            var profile_img = "https://frqevnyaghrnmtccnerc.supabase.co/storage/v1/object/public/mate-bucket/"+ data.image_url;
+        }
+        else{
+            var profile_img = "https://frqevnyaghrnmtccnerc.supabase.co/storage/v1/object/public/mate-bucket/profile/profile.jpg";
+        }
+        const profile = document.querySelector("#profile");
+        profile.src = profile_img;
+    }
+
+  document.getElementById("logout").addEventListener("click", async (event) => {
+    event.preventDefault();
+    await logout();
+    window.location.href = "../../index.html";
+  });
